bc2bcc8eb1ec9f8a38da0e4df6da13ddcb81f56f
169z87mn <169z87mn@gmail.com>
2021-10-03 01:18:25 +0900

    spotipy おためし

--- api/src/lambda_function.py
+++ api/src/lambda_function.py
@@ -1,7 +1,11 @@
 import json
+from .spotify import Spotify
 
 def lambda_handler(event, _context):
+    spotify = Spotify()
+    text = spotify.get_current_track_tweet_text()
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': text
     }
