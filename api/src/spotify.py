d7814bab7db4210638eb445fca8d4e9cc71fb908
169z87mn <169z87mn@gmail.com>
2021-10-04 18:23:03 +0900

    trackをdataclassに

--- api/src/spotify.py
+++ api/src/spotify.py
@@ -1,27 +1,15 @@
 import os
 import spotipy
+from src.track_data import Track
 
 class Spotify():
-    def __init__(self):
-        user_name = '169z87mn'
-        self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
-        self.client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
-        self.redirect_url = os.getenv('SPOTIFY_REDIRECT_URL')
-        self.scope = os.getenv('SPOTIFY_SCOPE')
-        token = spotipy.util.prompt_for_user_token(user_name, self.scope, self.client_id, self.client_secret, self.redirect_url)
-
+    def __init__(self, token: str):
         self.api = spotipy.Spotify(auth=token, language='ja')
 
-    def get_current_track_tweet_text(self) -> str:
+    def get_current_track(self) -> Track:
         current_track = self.api.current_user_playing_track()
-        # print(current_track)
-        playing_track = current_track['item']
-        artist_name = ', '.join([atst['name'] for atst in playing_track['artists']])
-        track_name = playing_track['name']
-        track_url = playing_track['external_urls']['spotify']
-        album_name = playing_track['album']['name']
-        # print(json.dumps(playing_track, indent=4))
-        tweet = f'{track_name} - {artist_name} #NowPlaying {track_url}'
-        # tweet = f'{track_name} - {artist_name} ({album_name}) #NowPlaying {track_url}'
-        print(tweet)
-        return tweet
+        track_item = current_track['item']
+        return Track(track_item['name'],
+                    ', '.join([atst['name'] for atst in track_item['artists']]),
+                    track_item['external_urls']['spotify'],
+                    track_item['album']['name'])
---
+++ api/src/track_data.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class Track:
+    title: str
+    artist: str
+    url: str
+    album: str
