feae0260192750dbf38e7097197ac39d41add80f
169z87mn <169z87mn@gmail.com>
2021-10-04 19:07:43 +0900

    codeで認証

--- api/src/spotify.py
+++ api/src/spotify.py
@@ -1,9 +1,20 @@
 import os
 import spotipy
+from spotipy import client
 from api.src.track_data import Track
 
 class Spotify():
-    def __init__(self, token: str):
+    def __init__(self, code: str):
+        self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
+        self.client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+        self.redirect_url = os.getenv('SPOTIFY_REDIRECT_URL')
+        self.scope = os.getenv('SPOTIFY_SCOPE')
+
+        spotify_oauth = spotipy.oauth2.SpotifyOAuth(client_id=self.client_id,
+                                                    client_secret=self.client_secret,
+                                                    redirect_uri=self.redirect_url,
+                                                    scope=self.scope)
+        token = spotify_oauth.get_access_token(code)
         self.api = spotipy.Spotify(auth=token, language='ja')
 
     def get_current_track(self) -> Track:
